a5bcccd87d350caee8130568f832d3bb050b9be9
liuzhao09 <liuzhao09@kuaishou.com>
2026-05-28 09:16:54 +0800

    Add 2026-05-28 market briefing

--- assets/js/site-index.js
+++ assets/js/site-index.js
@@ -86,6 +86,7 @@
   });
 
   const marketDates = Object.freeze({
+    "./market/2026-05-28.html": "2026-05-28",
     "./market/2026-05-27.html": "2026-05-27",
     "./market/2026-05-26.html": "2026-05-26",
   });
