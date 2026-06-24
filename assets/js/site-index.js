c5429848645aa8c4b6fe397c6c58cd06cc35303c
liuzhao09 <liuzhao09@kuaishou.com>
2026-06-24 10:57:35 +0800

    Add housing report for 2026-06-24

--- assets/js/site-index.js
+++ assets/js/site-index.js
@@ -218,6 +218,7 @@
   });
 
   const housingDates = Object.freeze({
+    "./housing/2026-06-24.html": "2026-06-24",
     "./housing/2026-06-19.html": "2026-06-19",
     "./housing/2026-06-12.html": "2026-06-12",
     "./housing/2026-06-05.html": "2026-06-05",
