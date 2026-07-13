f2984c72e196011454af33aafb836d37a5933008
liuzhao09 <liuzhao09@kuaishou.com>
2026-07-13 09:06:32 +0800

    Publish market briefing for 2026-07-13

--- assets/js/site-index.js
+++ assets/js/site-index.js
@@ -297,6 +297,7 @@
   });
 
   const marketDates = Object.freeze({
+    "./market/2026-07-13.html": "2026-07-13",
     "./market/2026-07-10.html": "2026-07-10",
     "./market/2026-07-09.html": "2026-07-09",
     "./market/2026-07-08.html": "2026-07-08",
