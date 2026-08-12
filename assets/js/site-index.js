eca5f72d72f7f703f997bf2d04817b9dcfa17611
liuzhao09 <liuzhao09@kuaishou.com>
2026-08-12 23:06:43 +0800

    Publish MetaStrategy paper note

--- assets/js/site-index.js
+++ assets/js/site-index.js
@@ -1,5 +1,6 @@
 (() => {
   const noteDates = Object.freeze({
+    "./papers/metastrategy.html": "2026-08-12",
     "./papers/specformer.html": "2026-08-12",
     "./papers/loopfm.html": "2026-08-12",
     "./papers/linkedin-feed-sr.html": "2026-08-12",
