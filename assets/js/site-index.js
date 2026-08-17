6f2e3c9cea183c17af1174e1b699fe893145d235
liuzhao09 <liuzhao09@kuaishou.com>
2026-08-17 09:06:41 +0800

    Add 2026-08-17 global market report

--- assets/js/site-index.js
+++ assets/js/site-index.js
@@ -443,6 +443,7 @@
   });
 
   const marketDates = Object.freeze({
+    "./market/2026-08-17.html": "2026-08-17",
     "./market/2026-08-14.html": "2026-08-14",
     "./market/2026-08-13.html": "2026-08-13",
     "./market/2026-08-12.html": "2026-08-12",
