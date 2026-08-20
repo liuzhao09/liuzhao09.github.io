88aa6cfb495bad4324b07069e54bd4e6002e6abe
刘钊 <liuzhao2@liuzhaodeMacBook-Pro.local>
2026-08-21 06:06:29 +0800

    Add housing analysis report for 2026-08-21

--- assets/js/site-index.js
+++ assets/js/site-index.js
@@ -505,6 +505,7 @@
   });
 
   const housingDates = Object.freeze({
+    "./housing/2026-08-21.html": "2026-08-21",
     "./housing/2026-08-14.html": "2026-08-14",
     "./housing/2026-08-07.html": "2026-08-07",
     "./housing/2026-07-17.html": "2026-07-17",
