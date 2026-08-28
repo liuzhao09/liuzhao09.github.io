38c25581ec7c13078082fefebf2522bd5de151c7
刘钊 <liuzhao2@liuzhaodeMacBook-Pro.local>
2026-08-28 09:06:43 +0800

    Sync global market report for 2026-08-28

--- assets/js/site-index.js
+++ assets/js/site-index.js
@@ -503,6 +503,7 @@
   });
 
   const marketDates = Object.freeze({
+    "./market/2026-08-28.html": "2026-08-28",
     "./market/2026-08-27.html": "2026-08-27",
     "./market/2026-08-26.html": "2026-08-26",
     "./market/2026-08-25.html": "2026-08-25",
