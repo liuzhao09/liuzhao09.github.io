de296de3c70beaa13f2e5e7e6add79002a6b3d13
刘钊 <liuzhao2@liuzhaodeMacBook-Pro.local>
2026-09-01 09:07:27 +0800

    Add 2026-09-01 market briefing

--- assets/js/site-index.js
+++ assets/js/site-index.js
@@ -517,6 +517,7 @@
   });
 
   const marketDates = Object.freeze({
+    "./market/2026-09-01.html": "2026-09-01",
     "./market/2026-08-31.html": "2026-08-31",
     "./market/2026-08-28.html": "2026-08-28",
     "./market/2026-08-27.html": "2026-08-27",
