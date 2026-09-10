7ddcac78c43964d60c3f5f92ec36251d98973e80
刘钊 <liuzhao2@liuzhaodeMacBook-Pro.local>
2026-09-11 06:12:26 +0800

    Publish September 11 housing analysis with August samples

--- assets/js/site-index.js
+++ assets/js/site-index.js
@@ -1,5 +1,6 @@
 (() => {
   const noteDates = Object.freeze({
+    "./housing/2026-09-11.html": "2026-09-11",
     "./papers/convmem.html": "2026-09-11",
     "./papers/kvsharearena.html": "2026-09-11",
     "./papers/compassopd.html": "2026-09-11",
