892647099ad6ccf11ff1d5e1893d8e9b3462094b
刘钊 <liuzhao2@liuzhaodeMacBook-Pro.local>
2026-09-15 09:10:45 +0800

    Add September 15 pre-open market briefing

--- assets/js/site-index.js
+++ assets/js/site-index.js
@@ -597,6 +597,7 @@
   });
 
   const marketDates = Object.freeze({
+    "./market/2026-09-15.html": "2026-09-15",
     "./market/2026-09-14.html": "2026-09-14",
     "./market/2026-09-11.html": "2026-09-11",
     "./market/2026-09-10.html": "2026-09-10",
