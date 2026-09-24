3d2f593dc8b0e5a8ee6233e7f781163c456fd42b
刘钊 <liuzhao2@liuzhaodeMacBook-Pro.local>
2026-09-25 06:15:13 +0800

    Publish housing analysis for 2026-09-25

--- assets/js/site-index.js
+++ assets/js/site-index.js
@@ -722,6 +722,7 @@
   });
 
   const housingDates = Object.freeze({
+    "./housing/2026-09-25.html": "2026-09-25",
     "./housing/2026-09-18.html": "2026-09-18",
     "./housing/2026-09-11.html": "2026-09-11",
     "./housing/2026-09-07.html": "2026-09-07",
