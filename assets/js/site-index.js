dc3a48bcdf68ca7e1570d44f528abc70f0d65daf
刘钊 <liuzhao2@liuzhaodeMacBook-Pro.local>
2026-09-25 09:11:02 +0800

    Publish September 25 market holiday briefing

--- assets/js/site-index.js
+++ assets/js/site-index.js
@@ -653,6 +653,7 @@
   });
 
   const marketDates = Object.freeze({
+    "./market/2026-09-25.html": "2026-09-25",
     "./market/2026-09-23.html": "2026-09-23",
     "./market/2026-09-22.html": "2026-09-22",
     "./market/2026-09-21.html": "2026-09-21",
